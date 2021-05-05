311c7c4f812293a54079b931f3ce6afd8e309c6a
Cole Lawrence <cole@colelawrence.com>
2021-05-05 19:56:17 -0400

    Prepare for 0.1.0 release

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,8 @@
+#![doc(
+    html_logo_url = "https://raw.githubusercontent.com/storyscript/layered-nlp/main/assets/layered-nlp.svg",
+    issue_tracker_base_url = "https://github.com/storyscript/layered-nlp/issues/"
+)]
+
 mod create_tokens;
 mod ll_line;
 mod resolvers;
@@ -6,7 +11,6 @@ mod type_id_to_many;
 
 #[cfg(test)]
 mod tests;
-
 pub use create_tokens::{create_tokens, InputToken};
 pub use ll_line::{
     x, FinishWith, LLCursorAssignment, LLLine, LLLineDisplay, LLSelection, Resolver, TextTag,
