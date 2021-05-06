daccb887dee5eab3914bc51f7540f9b76d56be74
Cole Lawrence <cole@colelawrence.com>
2021-05-05 20:03:35 -0400

    Add metadata for plugins

--- layered-clauses/src/lib.rs
+++ layered-clauses/src/lib.rs
@@ -1,3 +1,8 @@
+#![doc(
+    html_logo_url = "https://raw.githubusercontent.com/storyscript/layered-nlp/main/assets/layered-nlp.svg",
+    issue_tracker_base_url = "https://github.com/storyscript/layered-nlp/issues/"
+)]
+
 mod clause_keyword;
 mod clauses;
 
